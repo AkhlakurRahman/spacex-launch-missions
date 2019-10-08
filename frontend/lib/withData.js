2263bd3293de7f39059b47ae045a80516229ae5b
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-10-08 23:12:40 +0600

    configured for heroku deployment

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,11 +1,10 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
-import { endpoint } from '../config';
 
 export default withApollo(
   ({ ctx, headers, initialState }) =>
     new ApolloClient({
-      uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+      uri: '/graphql',
       cache: new InMemoryCache().restore(initialState || {})
     })
 );
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
+const path = require('path');
 
 const schema = require('./schema');
 
@@ -16,6 +17,12 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build'));
+});
 
 const PORT = process.env.BACKEND_PORT || 4444;
 
